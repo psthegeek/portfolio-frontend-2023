c19434bc17dcaf99feb447c0dd5ad4d780093f92
Preeti Singh <109643586+psthegeek@users.noreply.github.com>
2024-01-29 13:38:35 +0530

    Initial Commit

---
+++ data/data.js
@@ -0,0 +1,84 @@
+const Experience = [
+    {
+        id:1,
+        title: "Content Writer/Moderator",
+        jobType: "Freelancer",
+        skills: [
+            "Research skills", "Writing proficiency", "Adaptability", "Time Management", "Creativity", "Critical thinking","Editing and proofreading", "Decision Makeing Skills", "Communication Skills"
+        ],
+        workDetails: [
+            "Created and edited high-quality content for a variety of audiences, including blog posts, articles, social media posts, and website copy",
+            "Developed and implemented content strategies to increase website traffic and engagement",
+            "Optimized content for search engines (SEO)",
+            "Researched and wrote about a wide range of topics, including technology, business, and current events",
+            "Monitored and moderated user-generated content on social media, forums, and other online platforms",
+            "Enforced community guidelines and removed inappropriate content",
+            "Escalated potential safety or security risks to appropriate teams",
+            "Developed a content calendar to ensure consistent publishing and maintained a high editorial standard, resulting in increased reader engagement and retention",
+            "Utilized analytics tools to track content performance, identify trends, and make data-driven decisions to enhance content strategy and user experience",
+        ]
+    },
+    {
+        id:2,
+        title: "Web Developer",
+        jobType: "Freelancer",
+        skills: [
+            "Wordpress","WooCommerce", "Ajax", "HTML", "CSS", "Javascript", "PHP", "MySQL", "WooCommerce", "Project Management"
+        ],
+        workDetails: [
+                {
+                    id:1,
+                    projectName: "sg-lifestyle.com",
+                    projectDetails: [
+                        "Developed and maintained a WordPress ecommerce website using WooCommerce.",
+                        "Implemented product catalog management, including product descriptions, images, and pricing",
+                        "Configured payment gateways and shipping methods.",
+                        "Design a streamlined order processing workflow that guides customers through the checkout process seamlessly",
+                        "Integrate secure payment gateways to accept online payments from various credit cards and digital wallets.",
+                        "Provide order tracking and status updates to keep customers informed about their purchases.",
+                        "Set up tax rates and shipping calculations.",
+                        "Managed customer orders and inquiries.",
+                        "Provided ongoing website maintenance and support."
+                    ],
+                }
+            ]
+                    
+            
+        },
+    {
+        id:3,
+        title: "Front End Developer",
+        skills: [
+            "ReactJS", "NextJS", "HTML", "CSS", "Javascript", "Git/GitBash", "Github", "Jest", "Problem-solving and analytical skills","UI/UX design principles", "Experience with agile development methodologies" , "Jira" 
+        ],
+        workDetails: [
+                {
+                    id:1,
+                    projectName: "mrkt365.com",
+                    projectDetails: [
+                        ""
+                    ]
+                }
+        ]
+    },
+    {
+        id:4,
+        title: "",
+        techStack: [
+            "", ""
+        ],
+        workDetails: [
+
+        ]
+    }
+]
+
+
+const projects = [
+    {
+        id:1,
+        projectName: ""
+    }
+]
+
+export {Experience, projects}
